3110ebb32f4ad1feafdd5012314b01ffa8d82e18
Daniel Vlasits <dvlasits@gmail.com>
2023-09-07 13:40:50 +0100

    dfs monadically

---
+++ tests/test.ml
@@ -0,0 +1,23 @@
+module List' = List;;
+open Search.Algo;;
+open Imp.Control;;
+open Imp.Data;;
+open Imp.Show;;
+
+
+
+let edges n = match n with 
+              | 0 -> [0;1;2]
+              | 1 -> [3;5;6;7;8;9]
+              | 2 -> [3]
+              | 3 -> [] 
+              | _ -> []
+
+
+
+let () = let path = dfs {Int} edges (fun n -> n = 3) 0 in 
+            print_endline (show path)
+
+let () = let edges' n = List'.map (fun x -> [x]) (edges n) in
+         let pred x = [x = 3] in 
+         print_endline (show (dfsM {Int} {Imp.Control.List} edges' pred 0))
